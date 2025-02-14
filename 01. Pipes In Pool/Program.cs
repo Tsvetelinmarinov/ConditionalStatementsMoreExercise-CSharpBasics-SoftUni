aa807f4b4c6fecb0f9639bbc53dcc1223e18db01
Цветелин Маринов <Cvetelinmarinov566@gmail.com>
2025-02-14 20:54:22 +0200

    Add project files.

---
+++ 01. Pipes In Pool/Program.cs
@@ -0,0 +1,51 @@
+﻿/**
+ * 01. Pipes In Pool
+ */
+
+
+
+
+//Вход
+//От конзолата се четат четири реда:
+//•	Първият ред съдържа числото V – Обем на басейна в литри – цяло число в интервала [1…10000].
+//•	Вторият ред съдържа числото P1 – дебит на първата тръба за час – цяло число в интервала [1…5000].
+//•	Третият ред съдържа числото P2 – дебит на втората тръба за час– цяло число в интервала [1…5000].
+//•	Четвъртият ред H – часовете които работникът отсъства – реално число в интервала [1.0…24.00]
+
+//Изход
+//Да се отпечата на конзолата едно от двете възможни състояния:
+//•	До колко се е запълнил басейна и коя тръба с колко процента е допринесла.
+//o	"The pool is {запълненост на басейна в проценти}% full. Pipe 1: {процент вода от първата тръба}%. Pipe 2: {процент вода от втората тръба}%."
+//Aко басейнът се е препълнил – с колко литра е прелял за даденото време.
+//o	"For {часовете, които тръбите са пълнили вода} hours the pool overflows with {литрите вода в повече} liters."
+
+
+
+int poolVolume = int.Parse(Console.ReadLine());
+int leftPipeDebit = int.Parse(Console.ReadLine());
+int rigthPipeDebit = int.Parse(Console.ReadLine());
+double missingHours = double.Parse(Console.ReadLine());
+
+
+
+double totalForLeftPipe = leftPipeDebit * missingHours;
+double totalForRigthPipe = rigthPipeDebit * missingHours;
+
+
+double totalVolumeInto = totalForLeftPipe + totalForRigthPipe;
+
+double volumeIntoPercentage = totalVolumeInto / poolVolume * 100;
+double leftPipePercentage = totalForLeftPipe / totalVolumeInto * 100;
+double rigthPipePercentage = totalForRigthPipe / totalVolumeInto * 100;
+
+
+
+
+if (totalVolumeInto <= poolVolume)
+{
+    Console.WriteLine($"The pool is {volumeIntoPercentage:F2}% full. Pipe 1: {leftPipePercentage:F2}%. Pipe 2: {rigthPipePercentage:F2}%.");
+}
+else
+{
+    Console.WriteLine($"For {missingHours:F2} hours the pool overflows with {totalVolumeInto - poolVolume:F2} liters.");
+}
---
+++ 02. Sleepy Tom Cat/Program.cs
@@ -0,0 +1,45 @@
+﻿/**
+ * 02. Sleepy Tom Cat
+ */
+
+
+
+
+int freeDays = int.Parse(Console.ReadLine());
+
+// 63 / 127
+
+int workingDays = 365 - freeDays;
+
+double minutesFromWorkingDays = workingDays * 63;
+double minutesFromFreeDays = freeDays * 127;
+
+
+double totalMinutes = minutesFromFreeDays + minutesFromWorkingDays;
+
+
+//конзолата трябва да се отпечатат два реда.
+//•	Ако времето за игра на Том е над нормата за текущата година:
+//o На първия ред отпечатайте: “Tom will run away”
+//o	 На втория ред отпечатайте разликата от нормата във формат:
+//   “{H} hours and {M} minutes more for play”
+//•	Ако времето за игра на Том е под нормата за текущата година:
+//o На първия ред отпечатайте: “Tom sleeps well”
+//o	 На втория ред отпечатайте разликата от нормата във формат:
+//   “{H} hours and {M} minutes less for play”
+
+
+double difference = Math.Abs(30000 - totalMinutes);
+
+
+
+if (totalMinutes <= 30000)
+{
+    Console.WriteLine("Tom sleeps well");
+    Console.WriteLine($"{Math.Floor(difference / 60)} hours and {difference % 60} minutes less for play");
+}
+else
+{
+    Console.WriteLine("Tom will run away");
+    Console.WriteLine($"{Math.Floor(difference / 60)} hours and {difference % 60} minutes more for play");
+}
